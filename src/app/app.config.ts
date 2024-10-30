eb633891b264ffe7d39c1384362693639e9083fd
Olena Rozhko <olena.rozhko@gdnext.com>
2024-10-30 13:25:43 +0200

    added form

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),     importProvidersFrom(NoopAnimationsModule)
+  ]
 };
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule, NgForOf, NgIf} from '@angular/common';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatInput} from '@angular/material/input';
+import {MatStep, MatStepper, MatStepperNext, MatStepperPrevious} from '@angular/material/stepper';
+
 
 @Component({
   selector: 'app-form',
-  standalone: true,
-  imports: [],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NgForOf,
+    MatFormField,
+    MatIconButton,
+    MatButton,
+    MatInput,
+    MatError,
+    MatLabel,
+    NgIf,
+    MatStepper,
+    MatStep,
+    MatStepperNext,
+    MatStepperPrevious
+  ],
+  standalone: true
 })
 export class FormComponent {
+  myForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      info: this.fb.array([this.createInfoGroup()]),
+    });
+  }
+
+  get info() {
+    return this.myForm.get('info') as FormArray;
+  }
+
+  createInfoGroup() {
+    return this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  addInfoGroup() {
+    this.info.push(this.createInfoGroup());
+  }
+
+  removeInfoGroup(index: number) {
+    this.info.removeAt(index);
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    }
+  }
 }
